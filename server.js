2064ffa47aeb4c2f3f873b83d96be5e6d5513596
godomainers-droid <godomainers@gmail.com>
2025-08-18 00:53:47 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+
+app.use(express.json());
+
+// Proxy all requests to Dynadot
+app.all('/dynadot/*', async (req, res) => {
+  const dynadotUrl = `https://api.dynadot.com/api3.json${req.url.replace('/dynadot', '')}`;
+  
+  const response = await fetch(dynadotUrl, {
+    method: req.method,
+    headers: req.headers,
+    body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
+  });
+  
+  const data = await response.json();
+  res.json(data);
+});
+
+app.listen(process.env.PORT || 3000);
